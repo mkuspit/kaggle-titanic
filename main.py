d4432e754d1b57659a04345e7eab841da2bb607a
Michal Kuspit <michal.kuspit@gmail.com>
2020-07-17 09:22:15 +0400

    Add main.py
    Added project structure

---
+++ main.py
@@ -0,0 +1,34 @@
+# Kaggle Competition
+# Titanic: Machine Learning from Disaster
+# https://www.kaggle.com/c/titanic/
+
+
+#%% Import packages
+
+
+
+#%% Read data in
+
+
+
+#%% Analyze data
+
+
+
+#%% Engineer features
+
+
+
+#%% Build models
+
+
+
+#%% Finalize model
+
+
+
+#%% Create final predictions
+
+
+
+#%% Export solution
